63ae7eb2410f2934cb1f6f883aaa070a4b90619b
camperitox <ml17017@ues.edu.sv>
2022-09-03 00:26:44 -0600

    se agregaron las rutas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,35 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { MenuComponent } from './components/menu/menu.component';
+import { DetalleComponent } from './components/detalle/detalle.component';
+import { BaseComponent } from './layouts/base/base.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: BaseComponent,
+    children: [
+      {
+        path: '',
+        redirectTo:'/menu',
+        pathMatch: 'full'
+      },
+      {
+        path: 'menu',
+        component: MenuComponent,
+      },
+      {
+        path: 'detalle',
+        component: DetalleComponent,
+      }
+    ]
+  },
+  {
+    path:'**',
+    redirectTo:'menu'
+  }
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MenuComponent } from './components/menu/menu.component';
+import { DetalleComponent } from './components/detalle/detalle.component';
+import { BaseComponent } from './layouts/base/base.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatToolbarModule} from '@angular/material/toolbar';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
+    DetalleComponent,
+    BaseComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatToolbarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
